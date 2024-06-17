b577ecdcc2ef644674679873df1ea7458c7418f0
@RajatUpadhyay2000 <rajat.vns.2000@gmail.com>
2024-06-17 12:52:40 +0530

    new code

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 const port = 8080;
 const {v4 : uuidv4} = require('uuid');
 const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 const path = require('path');
 app.use(express.urlencoded({extended : true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
-app.use(methodOverride("_method"));
+
 
 let posts = [
     {
@@ -58,21 +59,29 @@ app.get("/posts/:id",(req,res) => {
 })
 
 app.patch('/posts/:id',(req,res)=>{
-    let {id} = req.params;
-   // console.log(id);
-   let newContent = req.body.content;
-   let post = posts.find((p) => id === p.id);
-   post.content = newContent;
-   console.log(post);
-   //console.log(newContent);
+    let { id }  = req.params;
+    //console.log(id);
+    let newContent = req.body.content;
+    //console.log(newContent);
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    //res.redirect("/posts");
     res.send("Patch request working");
-  // res.redirect("/posts");
+  
 })
 
 app.get("/posts/:id/edit",(req,res) => {
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
-    res.render("edit.ejs", {post});
+    res.render("edit.ejs",{ post });
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+     posts = posts.filter((p) => id !== p.id);
+    //res.send("delete success");
+    res.redirect("/posts");
 })
 
 app.listen(port,function(){
